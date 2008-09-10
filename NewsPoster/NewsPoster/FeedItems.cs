72fb3862a4da246ea833d9827bd4472073e4207a
Andy Britcliffe <britman@gmail.com>
2008-09-10 16:47:31 +0100

    Added keywords for sources
    Signed-off-by: Andy Britcliffe <britman@gmail.com>

--- NewsPoster/NewsPoster/FeedItems.cs
+++ NewsPoster/NewsPoster/FeedItems.cs
@@ -9,31 +9,44 @@ namespace NewsPoster
     {
         public FeedItems()
         {       
-            string[] englandTags = { "England" };
+            string[] englandTags = { "England", "The FA" };
             this.Add(new FeedItem(new Uri("http://www.thefa.com/rss/england/rss.xml"), englandTags));
-            this.Add(new FeedItem(new Uri("http://www.goal442.com/feed/")));
-            this.Add(new FeedItem(new Uri("http://www.clubcall.com/rss-football.xml")));
-            this.Add(new FeedItem(new Uri("http://downloads.bbc.co.uk/podcasts/fivelive/5lfd/rss.xml")));
-            this.Add(new FeedItem(new Uri("http://feeds.teletext.co.uk/sport/football/scores/")));
-            string[] echo1Tags = { "Liverpool" };
+            string[] goal442Tags = { "Goal442" };
+            this.Add(new FeedItem(new Uri("http://www.goal442.com/feed/"),goal442Tags));
+            string[] clubcallTags = { "Club Call" };
+            this.Add(new FeedItem(new Uri("http://www.clubcall.com/rss-football.xml"), clubcallTags));
+            string[] teletextTags = { "Teletext" };
+            this.Add(new FeedItem(new Uri("http://feeds.teletext.co.uk/sport/football/scores/"), teletextTags));
+            string[] echo1Tags = { "Liverpool", "Liverpool Echo" };
             this.Add(new FeedItem(new Uri("http://www.liverpoolecho.co.uk/liverpool-fc/liverpool-fc-news/rss.xml"), echo1Tags));
-            string[] echo2Tags = { "Everton" };
+            string[] echo2Tags = { "Everton","Liverpool Echo" };
             this.Add(new FeedItem(new Uri("http://www.liverpoolecho.co.uk/everton-fc/everton-fc-news/rss.xml"), echo2Tags));
-            string[] manEveTags = { "Man Utd" };
+            string[] manEveTags = { "Man Utd", "Manchester Evening News" };
             this.Add(new FeedItem(new Uri("http://www.manchestereveningnews.co.uk/sport/football/manchester_united/rss.xml"), manEveTags));
-            string[] manEve1Tags = { "Manchester City" };
+            string[] manEve1Tags = { "Manchester City","Manchester Evening News" };
             this.Add(new FeedItem(new Uri("http://www.manchestereveningnews.co.uk/sport/football/manchester_city/rss.xml"), manEve1Tags));
-            this.Add(new FeedItem(new Uri("http://fourfourtwo.com/rss/")));
-            this.Add(new FeedItem(new Uri("http://feeds.teletext.co.uk/sport/football/stories")));
-            this.Add(new FeedItem(new Uri("http://www.guardian.co.uk/football/rss")));
-            this.Add(new FeedItem(new Uri("http://www.telegraph.co.uk/newsfeed/rss/sport_football.xml")));
-            this.Add(new FeedItem(new Uri("http://www.sportinglife.com/rss/football.xml")));
-            this.Add(new FeedItem(new Uri("http://feeds.reuters.com/reuters/UKFootballNews"))); 
-            this.Add(new FeedItem(new Uri("http://www.teamtalk.com/rss/0,16039,2483,00.xml")));
-            this.Add(new FeedItem(new Uri("http://www.thesun.co.uk/sol/homepage/feeds/rss/article247739.ece")));
-            this.Add(new FeedItem(new Uri("http://www.mirror.co.uk/sport/football/rss.xml")));
-            this.Add(new FeedItem(new Uri("http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml")));
-            this.Add(new FeedItem(new Uri("http://www.skysports.com/rss/0,20514,11095,00.xml")));
+            string[] fourfourtwoTags = { "FourFourTwo" };
+            this.Add(new FeedItem(new Uri("http://fourfourtwo.com/rss/"), fourfourtwoTags));
+            //uses other teletextTags            
+            this.Add(new FeedItem(new Uri("http://feeds.teletext.co.uk/sport/football/stories"), teletextTags));
+            string[] guardianTags = { "The Guardian" };
+            this.Add(new FeedItem(new Uri("http://www.guardian.co.uk/football/rss"), guardianTags));
+            string[] telegraphTags = { "The Telegraph" };
+            this.Add(new FeedItem(new Uri("http://www.telegraph.co.uk/newsfeed/rss/sport_football.xml"), telegraphTags));
+            string[] sportinglifeTags = { "Sportinglife" };
+            this.Add(new FeedItem(new Uri("http://www.sportinglife.com/rss/football.xml"), sportinglifeTags));
+            string[] reutersTags = { "Reuters" };
+            this.Add(new FeedItem(new Uri("http://feeds.reuters.com/reuters/UKFootballNews"), reutersTags));
+            string[] teamTalkTags = { "TeamTalk" };
+            this.Add(new FeedItem(new Uri("http://www.teamtalk.com/rss/0,16039,2483,00.xml"), teamTalkTags));
+            string[] sunTags = { "The Sun" };
+            this.Add(new FeedItem(new Uri("http://www.thesun.co.uk/sol/homepage/feeds/rss/article247739.ece"), sunTags));
+            string[] mirrorTags = { "The Mirror" };
+            this.Add(new FeedItem(new Uri("http://www.mirror.co.uk/sport/football/rss.xml"), mirrorTags));
+            string[] bbcTags = { "BBC Sport" };
+            this.Add(new FeedItem(new Uri("http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/football/rss.xml"),bbcTags));
+            string[] skyTags = { "Sky Sports" };
+            this.Add(new FeedItem(new Uri("http://www.skysports.com/rss/0,20514,11095,00.xml"),skyTags));
         }
     }
 }
